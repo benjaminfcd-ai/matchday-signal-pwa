05359897791d431736fff13d9d4cf7872210cedf
benjaminfcd-ai <benjaminfcd@gmail.com>
2026-09-08 14:53:28 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
         <meta name="theme-color" content="#0b0d12" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="Matchday" />
+        <meta name="apple-mobile-web-app-title" content="PL Signal" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
